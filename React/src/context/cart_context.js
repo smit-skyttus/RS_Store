bd12c01a27e4f8604cf5d4a37579e7a7de08b6bc
smit-skyttus <smit.skyttus@gmail.com>
2023-04-24 11:41:18 +0530

    cmt

--- React/src/context/cart_context.js
+++ React/src/context/cart_context.js
@@ -35,6 +35,7 @@ const CartProvider = ({children})=>{
     const clearCart =()=>{
         dispatch({type:"CLEAR_CART"})
     };
+    // hs
 
     //increment and decrement of product
     const setDecrease = (id)=>{
